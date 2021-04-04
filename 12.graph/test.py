aecd05e47c792fc0cd287f9484709751764d9599
smilejakdu <ash982416@gmail.com>
2021-04-04 23:32:03 +0900

    bfs/dfs

---
+++ 12.graph/test.py
@@ -0,0 +1,18 @@
+import collections
+
+def solution(numbers, target):
+    answer = 0
+    stack = collections.deque([(0, 0)])
+    while stack:
+        current_sum, num_idx = stack.popleft()
+        if num_idx == len(numbers):
+            if current_sum == target:
+                answer += 1
+        else:
+            number = numbers[num_idx]
+            stack.append((current_sum+number, num_idx + 1))
+            stack.append((current_sum-number, num_idx + 1))
+
+    return answer
+
+print(solution([1,1,1,1,1,] , 3))
