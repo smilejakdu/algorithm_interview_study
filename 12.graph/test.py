81cc1849a785f208bef20810ddd6591626bf3551
smilejakdu <ash982416@gmail.com>
2021-04-06 04:04:55 +0900

    dfs_phone

--- 12.graph/test.py
+++ 12.graph/test.py
@@ -1,21 +1,19 @@
-from typing import List
+graph = {
+  1: [2, 3, 4],
+  2: [5],
+  3: [5],
+  4: [],
+  5: [6, 7],
+  6: [],
+  7: [3],
+}
 
-def solution(nums: List[int]) -> List[List[int]]:
-  results = []
-  prev_elements = []
-  
-  def dfs(elements):
-    if len(elements) == 0:
-      results.append(prev_elements[:])
+def recursive_dfs(v, discovered=[]):
+  discovered.append(v)
+  for w in graph[v]:
+    if not w in discovered:
+      discovered = recursive_dfs(w, discovered)
+  return discovered
 
-    for e in elements:
-      next_elements = elements[:]
-      next_elements.remove(e)
-
-      prev_elements.append(e)
-      dfs(next_elements)
-      prev_elements.pop()
-  dfs(nums)
-  return results
-
-print(solution([1,2,3]))
+print(recursive_dfs(1))
+      
