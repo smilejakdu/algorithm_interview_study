efdcefce4482a1e38f9992af39a186c06d094dc9
smilejakdu <ash982416@gmail.com>
2021-04-23 04:33:22 +0900

    sort_148

--- test.py
+++ test.py
@@ -6,27 +6,35 @@ import sys
 import collections
 
 
-def solution(priorities, location):
-    queue = collections.deque(priorities)
-    count = 0
-
-    while len(queue) != 0:
-        max_pri = max(queue)
-        if location == 0:
-            if queue[0] < max_pri:
-                queue.rotate(-1)
-                location = len(queue) - 1
-            else:
-                return count + 1
-        else:
-            if queue[0] < max_pri:
-                queue.rotate(-1)
-                location -= 1
-            else:
-                queue.popleft()
-                location -= 1
-                count += 1
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 
-print(solution([1, 1, 9, 1, 1, 1], 0))
+class Solution:
+    # 두 정렬 리스트 병합
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if l1 and l2:
+            if l1.val > l2.val:
+                l1, l2 = l2, l1
+            l1.next = self.mergeTwoLists(l1.next, l2)
 
+        return l1 or l2
+
+    def sortList(self, head: ListNode) -> ListNode:
+        if not (head and head.next):
+            return head
+
+        # 런너 기법 활용
+        half, slow, fast = None, head, head
+        while fast and fast.next:
+            half, slow, fast = slow, slow.next, fast.next.next
+        half.next = None
+
+        # 분할 재귀 호출
+        l1 = self.sortList(head)
+        l2 = self.sortList(slow)
+
+        return self.mergeTwoLists(l1, l2)
