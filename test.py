35196ad2204e58c139be2f2e22651d738553a743
smilejakdu <ash982416@gmail.com>
2021-04-23 05:05:54 +0900

    sort_56

--- test.py
+++ test.py
@@ -5,36 +5,18 @@ import sys
 
 import collections
 
-
-# Definition for singly-linked list.
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+intervals = [[1, 3], [2, 6], [8, 10], [15, 18]]
 
 
-class Solution:
-    # 두 정렬 리스트 병합
-    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        if l1 and l2:
-            if l1.val > l2.val:
-                l1, l2 = l2, l1
-            l1.next = self.mergeTwoLists(l1.next, l2)
+def merge(intervals):
+    merged = []
+    for i in sorted(intervals, key=lambda x: x[0]):  # [[1, 3], [2, 6], [8, 10], [15, 18]]
+        # 첫번재 값을 기준으로 정렬을 해준다. 즉  1 , 2 , 8 , 15 를 정렬한다.
+        if merged and i[0] <= merged[-1][1]:  # 다음 아이템의 시작이 <= 이전아이템 끝보다 작거나 같다면
+            merged[-1][1] = max(merged[-1][1], i[1]) # 이전아이템 끝 = max( 이전아이템 끝 , 다음아이템끝 )
+        else:
+            merged += i,  # 콤마를 넣게 되면 중첩 리스트를 만들어 준다. merged += [i] 와 같다.
+    return merged
 
-        return l1 or l2
 
-    def sortList(self, head: ListNode) -> ListNode:
-        if not (head and head.next):
-            return head
-
-        # 런너 기법 활용
-        half, slow, fast = None, head, head
-        while fast and fast.next:
-            half, slow, fast = slow, slow.next, fast.next.next
-        half.next = None
-
-        # 분할 재귀 호출
-        l1 = self.sortList(head)
-        l2 = self.sortList(slow)
-
-        return self.mergeTwoLists(l1, l2)
+print(merge(intervals))
