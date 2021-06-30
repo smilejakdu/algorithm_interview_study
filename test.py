591a4f910c0dcdffa804d9cb142e22850550c13b
smilejakdu <ash982416@gmail.com>
2021-07-01 00:49:45 +0900

    08

--- test.py
+++ test.py
@@ -1,34 +1,11 @@
-import sys
-
-# N = int(sys.stdin.readline())
-# a, b, c = map(int, sys.stdin.readline().split())
-
-import collections
-from typing import List
-
-# nums = [10, 2]
-nums = [3, 30, 34, 5, 9]
 from typing import List
 
 
 class Solution:
-    # 문제에 적합한 비교 함수
-    @staticmethod
-    def to_swap(n1: int, n2: int) -> bool:
-        return str(n1) + str(n2) < str(n2) + str(n1)  # 거꾸로 했을때 값이 더 크면 true ex ) 102 < 210
-
-    # 삽입 정렬 구현
-    def largestNumber(self, nums: List[int]) -> str:
-        i = 1
-        while i < len(nums):  # i 가 리스트 길이보다 작다면
-            j = i  # j 라는 변수에 넣는다.
-            while j > 0 and self.to_swap(nums[j - 1], nums[j]):  # j 가 0 보다 크고 to_swap(nums[j-1] , nums[j]) 가 true 이면
-                nums[j], nums[j - 1] = nums[j - 1], nums[j]  # swap 을 한다.
-                j -= 1  # j 가 1씩 작아진다.
-            i += 1  # i 가 1씩 커진다
-
-        return str(int(''.join(map(str, nums))))
+    def trap(self, height: List[int]) -> int:
+        return
 
 
+height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
 solution = Solution()
-print(solution.largestNumber(nums))
+solution.trap(height)
