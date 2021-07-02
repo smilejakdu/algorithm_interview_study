8c392d2ba3c7f4a6317a8f16809a7486f4093a33
smilejakdu <ash982416@gmail.com>
2021-07-02 14:14:36 +0900

    11

--- test.py
+++ test.py
@@ -1,12 +1,19 @@
 from typing import List
 
 
-class Solution(object):
-    def arrayPairSum(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        nums.sort()
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        out = []
+        p = 1
 
-        return sum(sorted(nums)[::2])
+        # 왼쪽 곱셈
+        for i in range(0, len(nums)):
+            out.append(p)
+            p = p * nums[i]
+
+        p = 1
+        # 왼쪽 곱셈 결과에 오른쪽 값을 차례대로 곱셈
+        for i in range(len(nums) - 1, -1, -1):
+            out[i] = out[i] * p
+            p = p * nums[i]
+        return out
