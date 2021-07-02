ac388cb5d6f17adefbbd7801ea5e25f5395f664e
smilejakdu <ash982416@gmail.com>
2021-07-02 09:31:56 +0900

    08 logic

--- test.py
+++ test.py
@@ -1,11 +1,29 @@
 from typing import List
 
+height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+
 
 class Solution:
     def trap(self, height: List[int]) -> int:
-        return
+        if not height:
+            return 0
+
+        volume = 0  # debug
+        left, right = 0, len(height) - 1
+        left_max, right_max = height[left], height[right]
+
+        while left != right:
+            left_max, right_max = max(height[left], left_max), max(height[right], right_max)
+            # 더 높은 쪽을 향해 투 포인터 이동
+            if left_max <= right_max:
+                volume += left_max - height[left]
+                left += 1
+            else:
+                volume += right_max - height[right]
+                right -= 1  # debug
+
+        return volume
 
 
-height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
 solution = Solution()
-solution.trap(height)
+print(solution.trap(height))
