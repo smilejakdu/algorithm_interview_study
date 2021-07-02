25a2c041417dd39f4ef457ca961f8d3b1a799785
smilejakdu <ash982416@gmail.com>
2021-07-02 13:16:22 +0900

    10

--- test.py
+++ test.py
@@ -1,29 +1,12 @@
 from typing import List
 
-height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
 
+class Solution(object):
+    def arrayPairSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
 
-class Solution:
-    def trap(self, height: List[int]) -> int:
-        if not height:
-            return 0
-
-        volume = 0  # debug
-        left, right = 0, len(height) - 1
-        left_max, right_max = height[left], height[right]
-
-        while left != right:
-            left_max, right_max = max(height[left], left_max), max(height[right], right_max)
-            # 더 높은 쪽을 향해 투 포인터 이동
-            if left_max <= right_max:
-                volume += left_max - height[left]
-                left += 1
-            else:
-                volume += right_max - height[right]
-                right -= 1  # debug
-
-        return volume
-
-
-solution = Solution()
-print(solution.trap(height))
+        return sum(sorted(nums)[::2])
